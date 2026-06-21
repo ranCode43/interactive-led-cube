9f29533fcf5424a206a292446da18dd4d8a3cc34
ranCode43 <ranengizaw@gmail.com>
2026-06-21 13:51:04 -0400

    Updating CAD and Firmware ReadME

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -3,6 +3,12 @@
 void setup() {
   Serial.begin(115200);
   Serial.println("Interactive LED Cube starting...");
+
+   /* TODO
+    INTILIZE W2812b LED Matrices
+     INTILIZE MAX4466 microphone
+     Intilize MMPU6050 sensor
+     INitlaize push button*/
 }
 
 void loop() {
